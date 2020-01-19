96ffdf33ab75b5fca2b8efd0ed676f3eada963ca
Isaacg94 <7248zack@gmail.com>
2020-01-19 21:36:15 +0300

    add navbar and static css and js files for improved page styling

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def landing(request):
-    return render(request, 'landing.html')
+    title = 'Home'
+    return render(request, 'landing.html',{"title":title})
