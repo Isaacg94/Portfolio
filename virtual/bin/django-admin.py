c0d9d9579e936db1a93d72e0f6157fad667fd3ab
Isaacg94 <7248zack@gmail.com>
2020-01-19 14:17:46 +0300

    initialize django environment and first template

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/isaac/Documents/Moringa-school-projects/pro-dev/portfolio/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
